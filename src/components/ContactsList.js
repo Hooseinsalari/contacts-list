45a104bfb149d34ba49425599887b23325808529
Hooseinsalari <hossein.salari002@gmail.com>
2022-06-10 01:32:43 +0430

    add a alert for contacts list when list is empty

--- src/components/ContactsList.js
+++ src/components/ContactsList.js
@@ -6,35 +6,45 @@ import styles from "./ContactsList.module.css";
 // icons
 import trash from "../svg/trash-can-solid.svg";
 import edit from "../svg/pen-to-square-solid.svg";
+import { Link } from "react-router-dom";
 
 const ContactsList = ({ contacts, deleteHandler }) => {
   return (
     <div className={styles.contList}>
-      {contacts.map((contact) => (
-        <div className={styles['contList__item']} key={contact.id}>
-          <div className={styles['contList__name']}>
-            <p>{contact.name}</p>
-          </div>
+      {contacts.length ? (
+        contacts.map((contact) => (
+          <div className={styles["contList__item"]} key={contact.id}>
+            <div className={styles["contList__name"]}>
+              <p>{contact.name}</p>
+            </div>
 
-          <div className={styles['contList__data']}>
-            <p className={styles['contList__number']}>
-              {contact.number}
-            </p>
-            <p className={styles['contList__email']}>
-              {contact.email}
-            </p>
-          </div>
+            <div className={styles["contList__data"]}>
+              <p className={styles["contList__number"]}>{contact.number}</p>
+              <p className={styles["contList__email"]}>{contact.email}</p>
+            </div>
 
-          <div className={styles['contList__btns']}>
-              <button className={styles['contList__trash-btn']} onClick={() => deleteHandler(contact.id)}>
+            <div className={styles["contList__btns"]}>
+              <button
+                className={styles["contList__trash-btn"]}
+                onClick={() => deleteHandler(contact.id)}
+              >
                 <img src={trash} alt="trash" />
               </button>
-              <button className={styles['contList__edit-btn']} onClick={() => deleteHandler(contact.id)}>
+              <button
+                className={styles["contList__edit-btn"]}
+                onClick={() => deleteHandler(contact.id)}
+              >
                 <img src={edit} alt="edit" />
               </button>
+            </div>
           </div>
+        ))
+      ) : (
+        <div className={styles['contList__alert']}>
+          <h2>لیستت خالیه!</h2>
+          <Link to="/add-contact">اضافه کردن مخاطب</Link>
         </div>
-      ))}
+      )}
     </div>
   );
 };
